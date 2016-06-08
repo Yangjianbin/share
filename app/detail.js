7499d7736f10ff49ea25f9ba334f03c7db478b3f
yangjianibn <851110276@qq.com>
2016-06-09 00:55:35 +0800

    mod

--- app/detail.js
+++ app/detail.js
@@ -1,17 +1,20 @@
 require('./detail.scss');
 var $ = require('jquery');
-var carousel = require('./components/carousel');
-carousel.init();
 var Vue = require('vue');
 var vueResource = require('vue-resource');
 Vue.use(vueResource);
+Vue.filter('datetime', function (value) {
+  var date = new Date();
+  date.setTime(value*1000);
+   return  (1+date.getMonth())+' 月 '+date.getDate()+' 日  '+date.getHours()+':'+date.getMinutes();
+})
 var vm = new Vue({
     el:'#app',
     ready:function(){
         $.ajax({
           url:'https://mall.uboxs.com/api/item/detail?type=json',
           type:'post',
-          data:{id:19},
+          data:{id:20},
           dataType:'json',
           success:function(d){
              console.log(d.result);
@@ -21,6 +24,7 @@ var vm = new Vue({
              // console.log(e)
           }
         })
+
         // this.$http.post('https://mall.uboxs.com/api/item/detail?type=json', {id:19}, {
         //     headers: {
         //         "X-Requested-With": "XMLHttpRequest"
@@ -33,6 +37,32 @@ var vm = new Vue({
         // });
     },
     data:{
-        
+
     }
 })
+
+var vm2 = new Vue({
+    el:'#app2',
+    ready:function(){
+        $.ajax({
+          url:'https://v3.api.uboxs.com/item/reply/list?type=json',
+          type:'post',
+          data:{item_id:20,page:1,limit:20},
+          dataType:'json',
+          success:function(d){
+             console.log(d.result);
+             vm2.$data = d.result;
+          },
+          error:function(e){
+             // console.log(e)
+          }
+        })
+
+    },
+    data:{
+
+    }
+})
+
+var carousel = require('./components/carousel');
+carousel.init();
--- app/surplus.js
+++ app/surplus.js
@@ -1,3 +1,62 @@
 require('./surplus.scss')
+var $ = require('jquery');
+var Vue = require('vue');
+var vueResource = require('vue-resource');
+Vue.use(vueResource);
+Vue.filter('datetime', function (value) {
+  var date = new Date();
+  date.setTime(value*1000);
+  return  (1+date.getMonth())+' 月 '+date.getDate()+' 日  '+date.getHours()+':'+date.getMinutes();
+});
+Vue.filter('lastMinutes', function (value) {
+    var d = new Date(value), now = new  Date(),str;
+    if( now.getFullYear() - d.getFullYear()>0){
+        str = now.getFullYear() - d.getFullYear() + ' 年';
+    }else if(now.getMonth() - d.getMonth()>0){
+        str = now.getMonth() - d.getMonth() + ' 月';
+    }else if(now.getDate() - d.getDate()>0){
+        str = now.getDate() - d.getDate() + ' 天';
+    }else if(now.getHours() - d.getHours() >0){
+        str = now.getHours() - d.getHours() + ' 小时';
+    }else{
+        str = now.getMinutes() - d.getMinutes() + '分钟';
+    }
+    return str;
+})
+
+var vm = new Vue({
+    el:'#app',
+    ready:function(){
+        $.ajax({
+          url:'http://v2.api.boxbuy.cc/getItemDetail?type=json',
+          type:'post',
+          data:{itemid:21479},
+          dataType:'json',
+          success:function(d){
+
+              $.ajax({
+                url:'https://api.uboxs.com/basic/schools?type=json&assoc=true&assign=schools',
+                type:'get',
+                dataType:'json',
+                success:function(schools){
+                    var school = schools.result[d.Item.schoolid]['name'];
+                    var location = schools.result[d.Item.schoolid]['campuses'][d.Item.location]['name'];
+                    d.school_location = school + location;
+                    vm.$data = d;
+                }
+              })
+
+          },
+          error:function(e){
+             // console.log(e)
+          }
+        })
+
+    },
+    data:{
+
+    }
+})
+
 var carousel = require('./components/carousel');
 carousel.init();
