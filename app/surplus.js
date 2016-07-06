99fd5e8c13f340f2bf45bc41d57e0f0790aca6c4
jianbinYang <851110276@qq.com>
2016-07-06 13:39:12 +0800

    add

--- app/surplus.js
+++ app/surplus.js
@@ -143,6 +143,35 @@ $('.story').on('click','a',function(e){
     });
 })
 
+$('.open-btn').click(function () {
+    var ua = navigator.userAgent.toLowerCase();
+    var t;
+    var config = {
+        scheme_IOS: 'ygzp://j?t=item&id=21479',
+        scheme_Adr: 'ygzp://j?t=item&id=1008',
+        download_url: 'http://app.uboxs.com/download.php',
+        timeout: 600
+    };
+
+    var startTime = Date.now();
+    var ifr = document.createElement('iframe');
+    ifr.src = ua.indexOf('os') > 0 ? config.scheme_IOS : config.scheme_Adr;
+    ifr.style.display = 'none';
+    document.body.appendChild(ifr);
+    var t = setTimeout(function() {
+        var endTime = Date.now();
+        if (!startTime || endTime - startTime < config.timeout + 200) { //如果装了app并跳到客户端后，endTime - startTime 一定> timeout + 200
+            window.location = config.download_url;
+        } else {
+
+        }
+    }, config.timeout);
+    window.onblur = function() {
+        clearTimeout(t);
+    }
+
+})
+
 var gAudioContext = new AudioContext();
 
 function E(selector) {
